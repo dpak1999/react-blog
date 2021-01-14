5f284caa51b3a0f6d56fe61b433e666dc3320333
dpak1999 <deepakkumardash313@gmail.com>
2021-01-14 18:18:38 +0530

    add dummy data

---
+++ src/data.js
@@ -0,0 +1,56 @@
+/** @format */
+
+export const blog = [
+  {
+    id: 1,
+    title: "Who is reinhardt??",
+    description:
+      "Sit excepteur pariatur minim fugiat commodo aliqua magna tempor voluptate sint labore exercitation reprehenderit.",
+    date: Date.now(),
+    author: "Bob willis",
+    tags: ["react", "js", "python"],
+    content:
+      "Laborum sit nulla veniam enim incididunt incididunt nostrud ut elit amet ad. Aute dolor minim proident eiusmod. Cupidatat magna quis tempor velit nisi dolor aute adipisicing aliqua enim. Ipsum qui fugiat magna quis aute duis fugiat pariatur qui adipisicing minim dolore dolore aliqua.",
+  },
+  {
+    id: 2,
+    title: "The stoneman that walked.",
+    description: "Sint labore cillum non occaecat aliqua ut deserunt aliqua.",
+    date: Date.now(),
+    author: "Tom hanks",
+    tags: ["java", "flutter", "go"],
+    content:
+      "Reprehenderit quis esse non sint ipsum officia in est amet labore. Culpa aliqua cillum voluptate occaecat do dolor reprehenderit aliquip. Sunt deserunt deserunt sit do.",
+  },
+  {
+    id: 3,
+    title: "That is why it happens!",
+    description: "Aliquip dolore laborum Lorem laboris elit amet.",
+    date: Date.now(),
+    author: "Enrique iglesis",
+    tags: ["kotlin", "c++", "c#"],
+    content:
+      "Dolor ad dolor occaecat nisi incididunt sunt dolor consequat nisi veniam laboris deserunt. Est consectetur occaecat amet ullamco excepteur pariatur exercitation aliquip ex in aute anim. Ea ea irure est sit est dolor esse incididunt tempor non minim elit incididunt. Cupidatat id culpa minim aute ipsum esse id enim est. Nostrud culpa duis anim occaecat qui.",
+  },
+  {
+    id: 4,
+    title: "Thus it thou!!!",
+    description: "In reprehenderit voluptate ad mollit fugiat.",
+    date: Date.now(),
+    author: "Jason Lafrey",
+    tags: ["os", "algorithms"],
+    content:
+      "Nisi nulla incididunt minim exercitation Lorem aliqua deserunt non pariatur in. Lorem cupidatat aliquip cillum eu exercitation quis. Consectetur ex dolore sunt irure dolore amet ullamco officia voluptate. Magna reprehenderit veniam elit sunt occaecat occaecat eu sunt consequat. Adipisicing adipisicing sint amet velit elit dolor.",
+  },
+  {
+    id: 5,
+    title: "The goodwill of catapult....",
+    description:
+      "Nulla nulla anim esse labore aliquip est deserunt ipsum ullamco anim exercitation duis mollit.",
+    date: Date.now(),
+    author: "Dan somerhault",
+    tags: ["php", "api"],
+    content:
+      "Id ex esse in nisi ut dolor ea amet ea velit amet laborum. Sit incididunt elit consectetur irure mollit ad ad qui quis proident nostrud eu fugiat deserunt. Ad est labore et cillum commodo in irure incididunt anim ut elit minim. Exercitation duis incididunt Lorem eu irure ad. Adipisicing aute velit id deserunt tempor ad culpa commodo exercitation duis sit elit esse. Enim occaecat sunt officia adipisicing laborum sunt. Pariatur in veniam aliquip irure amet Lorem incididunt est anim velit.",
+  },
+];

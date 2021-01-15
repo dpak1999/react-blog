a42d9815b609e35b0b60d2d17d4fb5c424877b7e
dpak1999 <deepakkumardash313@gmail.com>
2021-01-15 16:49:55 +0530

    add :  single post component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import AddPostScreen from "./screens/AddPostScreen";
+import PostScreen from "./screens/PostScreen";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Route path="/" exact component={HomeScreen} />
       <Route path="/add" exact component={AddPostScreen} />
+      <Route path="/post/:id" exact component={PostScreen} />
     </Router>
   );
 }
---
+++ src/screens/PostScreen.js
@@ -0,0 +1,55 @@
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import { blog } from "../data";
+import CommentCard from "../components/CommentCard";
+import { Col, Container, Row, Image } from "react-bootstrap";
+import CommentForm from "../components/CommentForm";
+
+const PostScreen = ({ match }) => {
+  const blogId = match.params.id;
+  const [blogs, setBlogs] = useState([]);
+  console.log(blogs);
+
+  const searchArray = (id, array) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].id === parseInt(id)) {
+        return array[i];
+      }
+    }
+  };
+
+  useEffect(() => {
+    const searchedBlog = searchArray(blogId, blog);
+    if (searchedBlog) {
+      setBlogs(searchedBlog);
+    }
+  }, [blogId]);
+
+  return (
+    <Container>
+      <Row>
+        <Col lg={12}>
+          <h1 class="mt-4">{blogs.title}</h1>
+          <p class="lead">-{blogs.author}</p>
+
+          <p>{blogs.date}</p>
+
+          <hr />
+
+          <Image rounded fluid src="https://picsum.photos/1000/400" />
+          <hr />
+
+          <p class="lead">{blogs.content}</p>
+
+          <CommentForm />
+
+          <CommentCard />
+          <CommentCard />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default PostScreen;
